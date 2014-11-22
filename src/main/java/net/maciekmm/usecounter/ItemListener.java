16893ef923b59451cd90df6a653b114a8ca3e5ef
Maciej Mionskowski <business@maciekmm.net>
2014-11-22 12:49:47 +0100

    Added /countable remove

--- src/main/java/net/maciekmm/usecounter/ItemListener.java
+++ src/main/java/net/maciekmm/usecounter/ItemListener.java
@@ -3,6 +3,8 @@ package net.maciekmm.usecounter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import net.maciekmm.usecounter.UseCounter.Result;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
@@ -12,9 +14,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 /**
  * Represents a listener for events for specific @see{Material}
@@ -31,6 +40,8 @@ public class ItemListener implements Listener {
         ITEM_MAPPINGS.put(PlayerFishEvent.class, Arrays.asList(Material.FISHING_ROD));
         ITEM_MAPPINGS.put(EntityShootBowEvent.class, Arrays.asList(Material.BOW));
         ITEM_MAPPINGS.put(EntityDeathEvent.class, Arrays.asList(Material.DIAMOND_SWORD, Material.GOLD_SWORD, Material.IRON_SWORD, Material.WOOD_SWORD));
+        ITEM_MAPPINGS.put(PlayerDeathEvent.class, Arrays.asList(Material.DIAMOND_SWORD, Material.GOLD_SWORD, Material.IRON_SWORD, Material.WOOD_SWORD));
+
     }
 
     private final UseCounter plugin;
@@ -40,16 +51,22 @@ public class ItemListener implements Listener {
     }
 
     private void processEvent(Class event, Player player) {
+        if(!player.hasPermission("usecounter.count")) {
+            return;
+        }
         ItemStack is = player.getItemInHand();
         if (is.getType() == Material.AIR) {
             return;
         }
-
-        if (!ITEM_MAPPINGS.get(event).contains(is.getType())) {
+        List<Material> materials = ITEM_MAPPINGS.get(event);
+        if (materials == null || !materials.contains(is.getType())) {
             return;
         }
 
-        plugin.bumpUseName(is);
+        if(plugin.bumpUseName(is)==Result.NOT_COUNTABLE) {
+            plugin.setItemCountable(is);
+            plugin.bumpUseName(is);
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -84,12 +101,41 @@ public class ItemListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onEntityDeath(EntityDeathEvent event) {
-        if(!(event.getEntity().getKiller() instanceof Player)) {
+        if (!(event.getEntity().getKiller() instanceof Player)) {
             return;
         }
-        if(plugin.getCountOnlyOnPlayerKillsInSwords() && !(event.getEntity() instanceof Player)) {
-            return;            
+        if (plugin.getCountOnlyOnPlayerKillsInSwords() && !(event.getEntity() instanceof Player)) {
+            return;
         }
         processEvent(event.getClass(), event.getEntity().getKiller());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onCraft(CraftItemEvent event) {
+        if (event.getInventory().getType() != InventoryType.WORKBENCH || !plugin.shouldSuffixBeAutomaticallyAdded()) {
+            return;
+        }
+        plugin.setItemCountable(event.getCurrentItem());
+    }
+
+    //@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onRename(InventoryClickEvent event) {
+        if (event.getCurrentItem().getType() == Material.AIR || event.getInventory().getType() != InventoryType.ANVIL) {
+            return;
+        }
+        ItemMeta meta = event.getCurrentItem().getItemMeta();
+        Matcher matcher = plugin.isCountableItem(meta.getDisplayName());
+        if(matcher == null) {
+            return;
+        }
+        if(event.getSlotType() == SlotType.CRAFTING) {
+            //meta.setDisplayName(matcher.);
+        } else if(event.getSlotType() == SlotType.RESULT) {
+            
+        }
+    }
+    
+    private boolean isPut(InventoryAction action) {
+        return action==InventoryAction.PLACE_ALL || action==InventoryAction.PLACE_ONE || action==InventoryAction.PLACE_SOME;
     }
 }
--- src/main/java/net/maciekmm/usecounter/UseCounter.java
+++ src/main/java/net/maciekmm/usecounter/UseCounter.java
@@ -15,28 +15,29 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UseCounter extends JavaPlugin implements Listener {
-
+    
     public enum Result {
-
+        
         NOT_COUNTABLE(ChatColor.RED + "Item you are holding is not supported."), ALREADY_COUNTABLE(ChatColor.RED + "Your item is already countable."), SUCCESS(ChatColor.GREEN + "Successfully set your item to countable.");
-
+        
         private final String message;
-
+        
         private Result(String message) {
             this.message = message;
         }
-
+        
         public String getMessage() {
             return this.message;
         }
     }
-
+    
     private Pattern matcher;
     private int group;
     private String suffix;
     private boolean countOnlyOnPlayerKillsInSwords;
+    private boolean autoAddSuffix;
     private ItemListener itemListener;
-
+    
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
@@ -44,22 +45,39 @@ public class UseCounter extends JavaPlugin implements Listener {
         this.group = this.getConfig().getInt("group", 2);
         this.suffix = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("defaultSuffix", " &8[&2{number}&8]")).replace("{number}", "0");
         this.countOnlyOnPlayerKillsInSwords = this.getConfig().getBoolean("countOnlyOnPlayerKillsInSwords", true);
+        this.autoAddSuffix = this.getConfig().getBoolean("addSuffixWhenCrafting", false);
         this.itemListener = new ItemListener(this);
         this.getServer().getPluginManager().registerEvents(itemListener, this);
     }
-
+    
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("You have to be a player to cast this command.");
             return true;
         }
-        sender.sendMessage(this.setItemCountable(((Player) sender).getItemInHand()).getMessage());
+        Player player = (Player) sender;
+        if (args.length == 0) {
+            sender.sendMessage(this.setItemCountable(player.getItemInHand()).getMessage());
+        } else {
+            if (args[0].equalsIgnoreCase("remove")) {
+                ItemMeta meta = player.getItemInHand().getItemMeta();
+                Matcher mat = this.isCountableItem(meta.getDisplayName());
+                if (mat == null) {
+                    sender.sendMessage(ChatColor.RED + "Item you are holding has no counter.");
+                    return true;
+                }
+                meta.setDisplayName(mat.replaceAll(""));
+                player.getItemInHand().setItemMeta(meta);
+                sender.sendMessage(ChatColor.GREEN + "Your item is no longer countable.");
+            }
+        }
         return true;
     }
 
     /**
      * Represents if current item has item counter in name.
+     *
      * @param displayName - name to check
      * @return Matcher for displayName.
      */
@@ -73,24 +91,25 @@ public class UseCounter extends JavaPlugin implements Listener {
 
     /**
      * Sets item to countable state
+     *
      * @param is - item to set countable
      * @return Outcome of event, if it was successfull or not.
      */
     public Result setItemCountable(ItemStack is) {
         ItemMeta meta = is.getItemMeta();
-
+        
         if (meta == null) {
             return Result.NOT_COUNTABLE;
         }
-
+        
         if (isCountableItem(meta.getDisplayName()) != null) {
             return Result.ALREADY_COUNTABLE;
         }
-
+        
         if (!canBeCountable(is.getType())) {
             return Result.NOT_COUNTABLE;
         }
-
+        
         String newName = meta.getDisplayName();
         if (newName == null || newName.isEmpty()) {
             newName = WordUtils.capitalize(is.getType().toString().toLowerCase().replace('_', ' '));
@@ -102,6 +121,7 @@ public class UseCounter extends JavaPlugin implements Listener {
 
     /**
      * Checks whether specified @see{Material} is countable.
+     *
      * @param material - checked @see{Material}
      * @return whether or not @see{Material} is countable
      */
@@ -116,34 +136,39 @@ public class UseCounter extends JavaPlugin implements Listener {
 
     /**
      * Bumps a stat of item in name by one
+     *
      * @param is - ItemStack to bump count on.
      * @return if stat was bumped.
      */
-    public boolean bumpUseName(ItemStack is) {
+    public Result bumpUseName(ItemStack is) {
         ItemMeta meta = is.getItemMeta();
         if (meta == null) {
-            return false;
+            return Result.NOT_COUNTABLE;
         }
-
+        
         Matcher globalMatch = isCountableItem(meta.getDisplayName());
         if (globalMatch == null) {
-            return false;
+            return Result.NOT_COUNTABLE;
         }
-
+        
         meta.setDisplayName(new StringBuilder(meta.getDisplayName()).replace(globalMatch.start(this.group), globalMatch.end(this.group), String.valueOf(Integer.valueOf(globalMatch.group(this.group)) + 1)).toString());
         is.setItemMeta(meta);
-        return true;
+        return Result.SUCCESS;
     }
-
+    
     int getGroupToChange() {
         return group;
     }
-
+    
     Pattern getPatternMatcher() {
         return matcher;
     }
-
+    
     boolean getCountOnlyOnPlayerKillsInSwords() {
         return countOnlyOnPlayerKillsInSwords;
+    }
+    
+    boolean shouldSuffixBeAutomaticallyAdded() {
+        return autoAddSuffix;
     }
 }
